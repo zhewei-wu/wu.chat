624d5af0c34a80d392d0ad92a26e5a126c06a9f7
Zhewei Wu <zjw5063@gmail.com>
2022-04-30 14:34:22 -0400

    this deploys

--- lib/wu.chat-stack.ts
+++ lib/wu.chat-stack.ts
@@ -1,10 +1,8 @@
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import {
-  Distribution,
-  SecurityPolicyProtocol,
-  SSLMethod,
-} from "aws-cdk-lib/aws-cloudfront";
+import { Aws, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { Distribution } from "aws-cdk-lib/aws-cloudfront";
 import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { HostedZone } from "aws-cdk-lib/aws-route53";
 import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
@@ -18,16 +16,28 @@ export class WuChatStack extends Stack {
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    new BucketDeployment(this, "DeployWebsite", {
-      sources: [Source.asset("app/.output/public")],
-      destinationBucket: bucket,
+    new HostedZone(this, "HostedZone", {
+      zoneName: "wu.chat",
     });
 
-    new Distribution(this, "Distribution", {
+    const certificate = Certificate.fromCertificateArn(
+      this,
+      "ImportedCertificate",
+      `arn:aws:acm:us-east-1:${Aws.ACCOUNT_ID}:certificate/016aa238-6ed2-469c-acc7-d88ea6ccd6af`
+    );
+
+    const distribution = new Distribution(this, "Distribution", {
       defaultBehavior: {
         origin: new S3Origin(bucket),
       },
       defaultRootObject: "index.html",
+      domainNames: ["wu.chat"],
+      certificate,
+    });
+
+    new BucketDeployment(this, "DeployWebsite", {
+      sources: [Source.asset("app/.output/public")],
+      destinationBucket: bucket,
     });
   }
 }
